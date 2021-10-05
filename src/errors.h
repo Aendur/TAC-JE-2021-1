4e351c1ef6f01cfefe89d623ef9c05ebe5c73a61
Aendur <Aendur@users.noreply.github.com>
2021-10-05 09:42:42 -0300

    Update errors.h

--- src/errors.h
+++ src/errors.h
@@ -4,5 +4,6 @@
 #include <stdexcept>
 #include <string>
 
-#define MSG_UNIMPLEMENTED_ERROR  std::string(__FILE__) + "(" + std::to_string(__LINE__) + "): unimplemented | " + std::string(__FUNCTION__)
+#define MSG_UNIMPLEMENTED_ERR std::string(__FILE__) + "(" + std::to_string(__LINE__) + "): unimplemented | " + std::string(__FUNCTION__)
+#define MSG_INCOMPLETE_IMPLMT std::string(__FILE__) + "(" + std::to_string(__LINE__) + "): incomplete implementation | " + std::string(__FUNCTION__)
 #endif
