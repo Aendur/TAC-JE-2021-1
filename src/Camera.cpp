bb4d4891b3097abaf9a8f57ccfef00353b25dad7
Aendur <Aendur@users.noreply.github.com>
2021-10-07 11:44:35 -0300

    create Camera header

---
+++ src/Camera.cpp
@@ -0,0 +1,26 @@
+#include "Camera.h";
+#include "errors.h";
+
+/*
+private:
+	inline static GameObject * focus = nullptr;
+public:
+	static Vec2 pos;
+	static Vec2 speed;
+*/
+
+void Camera::Follow(GameObject * newFocus) {
+	throw std::logic_error(MSG_UNIMPLEMENTED_ERR);
+}
+
+void Camera::Unfollow(void) {
+	throw std::logic_error(MSG_UNIMPLEMENTED_ERR);
+}
+
+void Camera::Update(float dt) {
+	throw std::logic_error(MSG_UNIMPLEMENTED_ERR);
+}
+
+
+
+
---
+++ src/Camera.h
@@ -0,0 +1,20 @@
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "Vec2.h";
+
+class GameObject;
+class Camera {
+private:
+	inline static GameObject * focus = nullptr;
+
+public:
+	static Vec2 pos;
+	static Vec2 speed;
+
+	static void Follow(GameObject * newFocus);
+	static void Unfollow(void);
+	static void Update(float dt);
+};
+
+#endif
