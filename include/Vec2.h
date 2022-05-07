077cf8b955645cc75a5430b9474f676bbfed2ba0
Aen <eandur@gmail.com>
2022-05-07 05:50:33 -0300

    new vector operations

--- include/Vec2.h
+++ include/Vec2.h
@@ -11,11 +11,16 @@ public:
 	Vec2 (void) : x(0.0f), y(0.0f) {}
 	Vec2 (float x, float y) : x(x), y(y) {}
 	
-	Vec2 RotateBy(float deg);
-	Vec2 operator+(const Vec2 & other);
+	Vec2 RotateBy(float deg) const;
+	Vec2 operator+(const Vec2 & other) const;
+	Vec2 operator-(const Vec2 & other) const;
+	Vec2 operator-(void) const;
+	Vec2 operator*(float other) const;
 
 	friend std::ostream& operator<< (std::ostream&, const Vec2&);
 };
+
+Vec2 operator*(float op1, const Vec2 & op2);
 
 
 #endif
--- src/Vec2.cpp
+++ src/Vec2.cpp
@@ -5,13 +5,33 @@
 #define M_PI 3.14159265358979323846
 #endif
 
-Vec2 Vec2::RotateBy(float deg) {
+Vec2 Vec2::RotateBy(float deg) const {
 	double t = deg * M_PI / 180.0;
-	return Vec2((float)(x * cos(t) - y * sin(t)), (float)(y * cos(t) + x * sin(t)));
+	double ct = cos(t);
+	double st = sin(t);
+	double rx = x * ct - y * st;
+	double ry = y * ct + x * st;
+	return Vec2((float)rx, (float)ry);
 }
 
-Vec2 Vec2::operator+(const Vec2 & other) {
+Vec2 Vec2::operator+(const Vec2 & other) const {
 	return Vec2(this->x + other.x, this->y + other.y);
+}
+
+Vec2 Vec2::operator-(const Vec2 & other) const {
+	return Vec2(this->x - other.x, this->y - other.y);
+}
+
+Vec2 Vec2::operator-(void) const {
+	return Vec2(-this->x, -this->y);
+}
+
+Vec2 Vec2::operator*(float other) const {
+	return Vec2(this->x * other, this->y * other);
+}
+
+Vec2 operator*(float op1, const Vec2 & op2) {
+	return op2 * op1;
 }
 
 std::ostream& operator<< (std::ostream& os, const Vec2& vec) {
